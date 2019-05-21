36b5a21e1ea29974813ae38434bbae7eb7cbf9fc
hyunwoo <h.hyunwoo@gmail.com>
2019-05-21 11:08:40 +0900

    vue-cli3-firebase-example deploy & config 수정

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyBMymVG35QMFNKFJQSQM-qJl8wdYBTLdCA',
-  authDomain: 'vue-cli3-firebase-sample.firebaseapp.com',
-  databaseURL: 'https://vue-cli3-firebase-sample.firebaseio.com',
-  projectId: 'vue-cli3-firebase-sample',
-  storageBucket: 'vue-cli3-firebase-sample.appspot.com',
-  messagingSenderId: '1066911321705',
-  appId: '1:1066911321705:web:7499ae1b2820d8e3'
+  apiKey: '[YOUR KEY]',
+  authDomain: '[YOUR KEY]',
+  databaseURL: '[YOUR KEY]',
+  projectId: '[YOUR KEY]',
+  storageBucket: '[YOUR KEY]',
+  messagingSenderId: '[YOUR KEY]',
+  appId: '[YOUR KEY]'
 };
+
 console.error(
   'Settings file needs to be changed. Please delete the log after making changes'
 );
